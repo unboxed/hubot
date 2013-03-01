27c5e6821985e91503b162ced41e6c50ac172be5
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-03-01 12:15:18 +0200

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 mkdir hubot
 cd hubot
 git init && git remote add heroku git@heroku.com:ubxd-hubot.git && git pull heroku master
-git reset --hard && git pull heroku master
+git reset --hard && git pull heroku master && rm script/*
 cd ..
 make package
 cd hubot
